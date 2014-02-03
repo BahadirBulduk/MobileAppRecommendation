1b9b7a4ad6470226be3ef98dd27771ffd5956c0c
muratcanbur <muratcanbur@gmail.com>
2014-02-04 01:34:24 +0200

    TimerService eklendi

---
+++ MobileAppRecommendation/src/akilliyazilim/android/services/TimerService.java
@@ -0,0 +1,57 @@
+package akilliyazilim.android.services;
+
+import java.util.Calendar;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TimerService extends Service {
+	private Calendar mCalendar;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+
+		mCalendar = Calendar.getInstance();
+		/* zaman ayarlamalarýný yap */
+		mCalendar.set(mCalendar.get(Calendar.YEAR),
+				mCalendar.get(Calendar.MONTH),
+				mCalendar.get(Calendar.DAY_OF_MONTH));
+
+		mCalendar.set(Calendar.HOUR_OF_DAY, 15);
+		mCalendar.set(Calendar.MINUTE, 01);
+		mCalendar.set(Calendar.SECOND, 00);
+		mCalendar.set(Calendar.AM_PM, Calendar.PM);
+
+		// Intent receiverIntent = new Intent(TimerService.this,
+		// AlarmReceiver.class);
+		//
+		// PendingIntent pendingIntent = PendingIntent.getBroadcast(
+		// TimerService.this, 0, receiverIntent, 0);
+		//
+		// AlarmManager alarmManager = (AlarmManager)
+		// getSystemService(ALARM_SERVICE);
+		// /*
+		// * Alarm Manager set et (tetiklenecek zaman ve intent parametre olarak
+		// * ekleniyor)!
+		// */
+		// alarmManager.set(AlarmManager.RTC, mCalendar.getTimeInMillis(),
+		// pendingIntent);
+
+		return super.onStartCommand(intent, flags, startId);
+
+	}
+}
