a2ea9118421a350aee3bade801ecb4e851e40ec1
muratcanbur <muratcanbur@gmail.com>
2014-02-05 00:58:59 +0200

    UploadService DownloadPage
    UploadService DownloadPage eklendi

--- MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/DownloadPage.java
+++ MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/DownloadPage.java
@@ -2,15 +2,50 @@ package akilliyazilim.android.mobileapprecommendation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.example.mobileapprecommendation.R;
 
 public class DownloadPage extends Activity {
+
+	TextView textLink1, textLink2, textLink3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_download);
+
+		textLink1 = (TextView) findViewById(R.id.textLink1);
+		textLink2 = (TextView) findViewById(R.id.textLink2);
+		textLink3 = (TextView) findViewById(R.id.textLink3);
+
+		textLink1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		textLink2.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		textLink3.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 	}
 }
--- MobileAppRecommendation/src/akilliyazilim/android/services/UploadService.java
+++ MobileAppRecommendation/src/akilliyazilim/android/services/UploadService.java
@@ -2,6 +2,8 @@ package akilliyazilim.android.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 
 public class UploadService extends Service {
@@ -15,7 +17,20 @@ public class UploadService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		// TODO Auto-generated method stub
+
+		if (intent.getExtras() != null) {
+			NetworkInfo ni = (NetworkInfo) intent.getExtras().get(
+					ConnectivityManager.EXTRA_NETWORK_INFO);
+			if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
+				// internet baglantýsý mevcut
+
+			} else if (intent.getBooleanExtra(
+					ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
+				// ýnternet baglantýsý yok
+			}
+		}
 		return super.onStartCommand(intent, flags, startId);
+
 	}
 
 }
