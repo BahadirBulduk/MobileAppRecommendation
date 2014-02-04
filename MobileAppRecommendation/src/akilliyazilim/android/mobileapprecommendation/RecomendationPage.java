1c705b3a694116df12351aa8a74b1fa53fee2c95
muratcanbur <muratcanbur@gmail.com>
2014-02-04 21:11:06 +0200

    Recomendation güncellemesi
    Recomendation güncellemesi

--- MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/RecomendationPage.java
+++ MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/RecomendationPage.java
@@ -2,15 +2,41 @@ package akilliyazilim.android.mobileapprecommendation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.mobileapprecommendation.R;
 
 public class RecomendationPage extends Activity {
+
+	private Button button;
+	private Spinner spinnerAnket1, spinnerAnket2, spinnerAnket3;
+	String cevap1, cevap2, cevap3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recomendation);
+
+		spinnerAnket1 = (Spinner) findViewById(R.id.spinner1);
+		spinnerAnket2 = (Spinner) findViewById(R.id.spinner2);
+		spinnerAnket3 = (Spinner) findViewById(R.id.spinner3);
+		button = (Button) findViewById(R.id.button1);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				cevap1 = String.valueOf(spinnerAnket1.getSelectedItem());
+				cevap2 = String.valueOf(spinnerAnket2.getSelectedItem());
+				cevap3 = String.valueOf(spinnerAnket3.getSelectedItem());
+				/* Bu bilgiler burada db ye yazýlmalý */
+
+			}
+		});
 	}
 }
