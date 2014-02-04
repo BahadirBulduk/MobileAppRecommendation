1f939d8a927934d7116f294a6a34e11f53bbf5e8
muratcanbur <muratcanbur@gmail.com>
2014-02-04 20:48:19 +0200

    Activityler güncellendi

--- MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/DownloadPage.java
+++ MobileAppRecommendation/src/akilliyazilim/android/mobileapprecommendation/DownloadPage.java
@@ -3,11 +3,14 @@ package akilliyazilim.android.mobileapprecommendation;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.mobileapprecommendation.R;
+
 public class DownloadPage extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_download);
 	}
 }
--- MobileAppRecommendation/src/akilliyazilim/android/services/RecomendationService.java
+++ MobileAppRecommendation/src/akilliyazilim/android/services/RecomendationService.java
@@ -1,11 +1,18 @@
 package akilliyazilim.android.services;
 
+import akilliyazilim.android.mobileapprecommendation.RecomendationPage;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.example.mobileapprecommendation.R;
 
 public class RecomendationService extends Service {
+
 	private NotificationManager mManager;
 
 	@Override
@@ -22,22 +29,20 @@ public class RecomendationService extends Service {
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		// Intent intentNotif = new Intent(getBaseContext(),
-		// RecomendationPage.class);
-		// PendingIntent pIntent = PendingIntent.getActivity(this, 0,
-		// intentNotif,
-		// 0);
-		//
-		// Notification n = new NotificationCompat.Builder(this)
-		// .setContentTitle("Uygulama Baþlýðý ")
-		// .setContentText("Ufak Uygulama Yazýsý")
-		// .setSmallIcon(R.drawable.ic_launcher).setContentIntent(pIntent)
-		// .addAction(R.drawable.ic_launcher, "Call", pIntent).build();
-		// NotificationManager notificationManager = (NotificationManager)
-		// getSystemService(NOTIFICATION_SERVICE);
-		// // noti.flags |= Notification.FLAG_AUTO_CANCEL;
-		//
-		// notificationManager.notify(0, n);
+		Intent intentNotif = new Intent(getBaseContext(),
+				RecomendationPage.class);
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intentNotif,
+				0);
+		/* Notification yapýsý tanýmlanýyor */
+		Notification n = new NotificationCompat.Builder(this)
+				.setContentTitle("Uygulama Baþlýðý ")
+				.setContentText("Ufak Uygulama Yazýsý")
+				.setSmallIcon(R.drawable.ic_launcher).setContentIntent(pIntent)
+				.addAction(R.drawable.ic_launcher, "Call", pIntent).build();
+		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		// n.flags |= Notification.FLAG_AUTO_CANCEL; flag degýsmeli
+
+		notificationManager.notify(0, n);
 		return super.onStartCommand(intent, flags, startId);
 	}
 
